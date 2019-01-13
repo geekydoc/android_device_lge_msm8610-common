f5bf1ac795d5f9a6b2444d6048d8240aa692457d
Panos <Panos@localhost>
2019-01-13 11:52:53 +0200

    msm8610: Add BOARD_KERNEL_IMAGE_NAME

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -49,6 +49,7 @@ BOARD_KERNEL_SEPARATED_DT := true
 TARGET_KERNEL_SOURCE := kernel/lge/msm8610
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.selinux=enforcing user_debug=23 msm_rtb.filter=0x37 androidboot.hardware=msm8610
 BOARD_KERNEL_BASE := 0x00000000
+BOARD_KERNEL_IMAGE_NAME := zImage
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/linaro-4.9/bin
