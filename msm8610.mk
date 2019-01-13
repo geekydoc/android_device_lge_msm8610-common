a3d182c05163904732bb228e045f888d9424db1f
Panos <Panos@localhost>
2019-01-13 11:04:39 +0200

    msm8610: Update VM heap configuration

--- msm8610.mk
+++ msm8610.mk
@@ -16,9 +16,9 @@
 
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.heapstartsize=5m \
-    dalvik.vm.heapgrowthlimit=64m \
-    dalvik.vm.heapsize=72m \
-    dalvik.vm.heaptargetutilization=0.75 \
+    dalvik.vm.heapgrowthlimit=72m \
+    dalvik.vm.heapsize=94m \
+    dalvik.vm.heaptargetutilization=0.8 \
     dalvik.vm.heapminfree=512k \
     dalvik.vm.heapmaxfree=2m
 
